1bb2015893a7ad2c1bb8c452a71f8cdea5aa23da
Bittrance <anders.qvist@burtcorp.com>
2017-01-31 23:39:59 +0100

    - lock linear-regression to 0.0.2 - support float timeframes e.g. --projection 1.5days - style improvements as suggested by iconara on PR #1

--- spec/attime_spec.rb
+++ spec/attime_spec.rb
@@ -1,17 +1,20 @@
 require 'attime'
 
-describe '#attime' do
+describe 'CheckGraphite.attime' do
   subject do |example|
     delta = example.metadata[:description_args][0]
-    CheckGraphite.attime(delta)
+    CheckGraphite.attime(delta, now)
   end
 
+  let(:now) { Time.now }
+
   def timedelta(n)
-    Time.now + n
+    now + n
   end
 
   it("10days") { should be_within(0.9).of(timedelta(10 * 86400)) }
   it("0days") { should be_within(0.9).of(timedelta(0)) }
+  it("1.5days") { should be_within(0.9).of(timedelta(1.5 * 86400)) }
   it("-10days") { should be_within(0.9).of(timedelta(-10 * 86400)) }
   it("5seconds") { should be_within(0.9).of(timedelta(5)) }
   it("5minutes") { should be_within(0.9).of(timedelta(5 * 60)) }
@@ -26,6 +29,7 @@ describe '#attime' do
   it(1) { expect { subject }.to raise_error(/string/i) }
   it("Something") { expect { subject }.to raise_error(RuntimeError) }
   it("1m") { expect { subject }.to raise_error(/bad unit/i) }
+  it("1day 1day") { expect { subject }.to raise_error(/bad unit/i) }
   it("10") { expect { subject }.to raise_error(/unit/) }
   it("month") { expect { subject }.to raise_error(/scalar/) }
 end
--- spec/projection_spec.rb
+++ spec/projection_spec.rb
@@ -1,7 +1,7 @@
 require 'check_graphite/projection'
 require 'nagios_check'
 
-describe describe CheckGraphite::Projection do
+describe CheckGraphite::Projection do
   let :check do
     Class.new do
       attr_reader :values
@@ -10,7 +10,7 @@ describe describe CheckGraphite::Projection do
     end.new
   end
 
-  describe 'primary value' do
+  describe 'when looking at the primary value' do
     subject do
       check.prepare
       check.send(:parse_options, ['--projection', projection])
@@ -33,7 +33,7 @@ describe describe CheckGraphite::Projection do
     end
   end
 
-  describe 'p-value' do
+  describe 'when looking at the p-value' do
     subject do
       check.prepare
       check.send(:parse_options, ['--projection', projection])
@@ -54,27 +54,27 @@ describe describe CheckGraphite::Projection do
       it { should eq("1.0") }
     end
   end
+end
 
-  describe 'integration test' do
-    before do
-      FakeWeb.register_uri(
-        :get, "http://your.graphite.host/render?target=collectd.somebox.load.load.midterm&from=-30seconds&format=json",
-        :body => '[{"target": "collectd.somebox.load.load.midterm", "datapoints": [[1.0, 1339512060], [2.0, 1339512120], [6.0, 1339512180], [7.0, 1339512240]]}]',
-        :content_type => "application/json"
-      )
-    end
+describe 'when invoking graphite with --projection' do
+  before do
+    FakeWeb.register_uri(
+      :get, "http://your.graphite.host/render?target=collectd.somebox.load.load.midterm&from=-30seconds&format=json",
+      :body => '[{"target": "collectd.somebox.load.load.midterm", "datapoints": [[1.0, 1339512060], [2.0, 1339512120], [6.0, 1339512180], [7.0, 1339512240]]}]',
+      :content_type => "application/json"
+    )
+  end
 
-    it 'should output value, projection interval and p-value' do
-      stub_const("ARGV", %w{
-        -H http://your.graphite.host/render
-        -M collectd.somebox.load.load.midterm
-        -c 0:10
-        --projection 5min
-        --name ze-name
-      })
-      c = CheckGraphite::Command.new
-      STDOUT.should_receive(:puts).with(match(/ze-name.*in 5min.*p-value=0.9/))
-      lambda { c.run }.should raise_error SystemExit
-    end
+  it 'outputs value, projection interval and p-value' do
+    stub_const("ARGV", %w{
+      -H http://your.graphite.host/render
+      -M collectd.somebox.load.load.midterm
+      -c 0:10
+      --projection 5min
+      --name ze-name
+    })
+    c = CheckGraphite::Command.new
+    STDOUT.should_receive(:puts).with(match(/ze-name.*in 5min.*p-value=0.9/))
+    lambda { c.run }.should raise_error SystemExit
   end
 end
