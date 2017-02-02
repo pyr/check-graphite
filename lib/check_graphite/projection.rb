edca3de07188b8296f7fc504472d524083e253b5
Bittrance <anders.qvist@burtcorp.com>
2017-02-02 15:01:12 +0100

    Add --p-threshold which triggers status UNKNOWN

--- lib/check_graphite/projection.rb
+++ lib/check_graphite/projection.rb
@@ -10,6 +10,16 @@ module CheckGraphite
         options.send(:processor=, method(:projected_value))
         options.send(:timeframe=, timeframe)
       end
+
+      base.on '--p-threshold VALUE' do |raw_threshold|
+        begin
+          threshold = Float(raw_threshold)
+          raise "Expected 0 <= #{threshold} <= 1" unless threshold >= 0 && threshold <= 1
+          options.send(:p_threshold=, threshold)
+        rescue ArgumentError
+          raise "Expected #{raw_threshold} to be a float"
+        end
+      end
     end
 
     def projected_value(datapoints)
@@ -17,23 +27,32 @@ module CheckGraphite
       lr = Regression::Linear.new(xs, ys)
       future = CheckGraphite.attime(options.timeframe, xs[-1])
       value = lr.predict(future.to_i)
-      p = Regression::CorrelationCoefficient.new(xs, ys).pearson
-      store_value options.name, value
+      p = Regression::CorrelationCoefficient.new(xs, ys).pearson.abs
+      if is_good_p?(p)
+        store_value options.name, value
+        store_message "#{options.name}=#{format_float(value)} in #{options.timeframe}"
+      else
+        store_value options.name, nil
+        store_message "No projection on #{options.name}; p = #{format_float(p)} < #{options.p_threshold}"
+      end
       # Unfortunately, nagios_check converts both primary and
       # secondary values to float. Hence manual assignment instead:
-      @values['p-value'] = format_float(p.abs)
-      store_message "#{options.name}=#{format_float(value)} in #{options.timeframe}"
+      @values['p-value'] = format_float(p)
       return value
     end
 
     private
 
-     def format_float(v)
-       if v.nan?
-         'undefined'
-       else
-         BigDecimal.new(v, 3).to_s('F')
-       end
-     end
+    def is_good_p?(p)
+      options.p_threshold.nil? || p.nan? || p >= options.p_threshold
+    end
+
+    def format_float(v)
+      if v.nan?
+        'undefined'
+      else
+        BigDecimal.new(v, 3).to_s('F')
+      end
+    end
   end
 end
